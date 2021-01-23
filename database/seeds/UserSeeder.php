e738a2d614ec7a9f227f4a6c9651a9cb8dc00a6a
gigihridho <gigih.r.r@mail.ugm.ac.id>
2021-01-23 19:12:04 +0700

    first n role

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'griyakenyo@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'Andi',
+            'email' => 'andika@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        $user->assignRole('user');
+    }
+}
