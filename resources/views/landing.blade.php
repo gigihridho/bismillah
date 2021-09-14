9ee670b589e3570103879abd8334cd876b5a8937
gigihridho <gigih.r.r@mail.ugm.ac.id>
2021-09-14 09:45:53 +0700

    jquery

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -485,6 +485,7 @@
 </section>
 @endsection
 @push('after-script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
 <script src="{{ asset('fe/js/owl.carousel.js') }}"></script>
 <script src="{{ asset('fe/js/owl.carousel.min.js') }}"></script>
 <script>
