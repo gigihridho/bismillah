84367d19181eeb1230d1843623f99906c179f5a6
gigihridho <gigih.r.r@mail.ugm.ac.id>
2021-09-14 09:41:10 +0700

    carouse

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -491,17 +491,19 @@
     $('.featured-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
     responsiveClass:true,
     responsive:{
         0:{
-            items:1
+            items:1,
+            nav:true,
         },
         600:{
-            items:1
+            items:1,
+            nav:true,
         },
         1000:{
-            items:1
+            items:1,
+            nav:true,
         }
     }
 })
