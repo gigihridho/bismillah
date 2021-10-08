ab775e53d8d54062ccff2f3a5dae7a46259e6866
gigihridho <gigih.r.r@mail.ugm.ac.id>
2021-10-08 16:53:29 +0700

    up

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -12,7 +12,7 @@ class DetailController extends Controller
     public function detail(Request $request, $id)
     {
         $tipe_kamars = TipeKamar::where('id',$id)->get();
-        $fasilitas = TipeKamar::with('fasilitas')->where('status',true)->get();
+        $fasilitas = TipeKamar::with('fasilitas')->get();
         return view('detail-kost', [
             'tipe_kamars' => $tipe_kamars,
             'fasilitas' => $fasilitas,
