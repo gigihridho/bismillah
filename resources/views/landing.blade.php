e8b2177dd590d5756746073da4b3976b84bcfa9c
gigihridho <gigih.r.r@mail.ugm.ac.id>
2021-10-19 17:40:57 +0700

    review

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -364,7 +364,7 @@
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script> --}}
 <script src="{{ asset('fe/js/owl.carousel.js') }}"></script>
 <script src="{{ asset('fe/js/owl.carousel.min.js') }}"></script>
-<script>
+<script type="text/javascript">
 $(document).ready(function() {
 $("#owl-image").owlCarousel({
         autoplay:true,
@@ -381,7 +381,7 @@ $("#owl-image").owlCarousel({
                 nav:true,
             },
             1000:{
-                items:3,
+                items:1,
                 nav:true,
             }
         }
